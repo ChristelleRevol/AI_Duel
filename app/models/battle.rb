27a46ea3336c798052470470915d990903ce0b83
Christelle Revol <christellerevol.works@gmail.com>
2025-03-18 14:23:25 +0100

    fix bug has_many user

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,6 +1,7 @@
 class Battle < ApplicationRecord
   belongs_to :user
-  has_many :responses, :votes
+  has_many :responses
+  has_many :votes
 
   validates :category, :end_date, :prompt, presence: true
 end
