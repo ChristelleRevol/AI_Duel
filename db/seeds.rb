854993413c46f488a3eb9eb8413490c49b22c575
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-03-18 16:01:55 +0100

    installed faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+categories = ["research", "image generation", "content generation", "discussion", "mathematics"]
+
+puts "Cleaning DB..."
+User.destroy_all
+Battle.destroy_all
+Vote.destroy_all
+puts "Creating users..."
+User.create(
+  email: Faker::Internet.email,
+  pseudo: Faker::Internet.username,
+  password: "123456"
+)
+puts "Creating battles..."
+Battle.create(
+  category: categories.sample,
+  content_prompt: Faker::Lorem.paragraph(sentence_count: rand(2..5)),
+  status: Faker::Boolean.boolean(true_ratio: 0.2),
+  end_date: Faker::Date.between(from: 2.days.ago, to: 1.days.from_now),
+  winner: "",
+  user: ""
+)
+puts "Creating votes..."
+Vote.create(
+  user: "",
+  battle: "",
+  response: ""
+)
+puts "Creating responses..."
+Response.create(
+  model: "",
+  battle: "",
+  content: Faker::Lorem.paragraph(sentence_count: rand(5..10))
+)
+puts "Finished!\n
+Created #{User.count} users |#{Battle.count} battles | #{Response.count} responses | #{Vote.count} votes"
