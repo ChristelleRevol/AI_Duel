9a8db4ee0a2a042c27823ddf8b07639edd07ee89
Mathieu deTorres <mathieu.detorres@gmail.com>
2025-03-20 12:46:22 +0100

    commit before merge and redo

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -4,4 +4,30 @@ class Battle < ApplicationRecord
   has_many :votes
 
   validates :category, :end_date, :prompt, presence: true
+
+  def mistral_response
+    set_mistral_response if responses.find_by(model: "Mistral").nil?
+    responses.find_by(model: "Mistral")
+  end
+
+  private
+
+  def set_mistral_response
+    content = HTTParty.post(
+      "https://api.mistral.ai/v1/chat/completions",
+      headers: {
+        "Content-Type" => "application/json",
+        "Accept" => "application/json",
+        "Authorization" => "Bearer #{ENV.fetch('MISTRAL_API_KEY') { raise 'Clé API manquante !' }}"
+      },
+      body: {
+        model: "mistral-large-latest",
+        messages: [{ role: "user", content: prompt }]
+      }.to_json
+    )
+    puts "c'est la"
+    puts content
+    # content.parsed_response["choices"][0]["message"]["content"]
+    Response.create(model: "Mistral", content: content, battle: self)
+  end
 end
