5b3202a6751eb6f0fe88f82c620a028e86e43597
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-03-21 16:12:59 +0100

    did migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2025_03_21_115740) do
-=======
-ActiveRecord::Schema[7.1].define(version: 2025_03_21_115514) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
