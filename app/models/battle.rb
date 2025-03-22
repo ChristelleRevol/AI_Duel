418bd008d3255dde9f200a18099f2776a77c4f60
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-03-22 13:14:47 +0100

    new try

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -9,6 +9,12 @@ class Battle < ApplicationRecord
 
   private
 
+  def create_responses
+    Response.create(model: "Claude", battle: self)
+    Response.create(model: "OpenAI", battle: self)
+    Response.create(model: "Mistral", battle: self)
+  end
+
   def async_responses_call
     AskClaudeJob.perform_later(self)
     AskOpenAiJob.perform_later(self)
