3ab4f30417c678f1cb7219e867af63e952ea3fc1
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-03-22 13:21:59 +0100

    still testing

--- app/jobs/ask_claude_job.rb
+++ app/jobs/ask_claude_job.rb
@@ -2,14 +2,17 @@ class AskClaudeJob < ApplicationJob
   queue_as :default
 
   def perform(battle)
-    client = Anthropic::Client.new
-    content = client.messages(
-      parameters: {
-        model: "claude-3-haiku-20240307",
-        messages: [{ role: "user", content: battle.prompt }],
-        max_tokens: 1000
-      }
-    )['content'][0]['text']
-    Response.create(model: "Claude", content: content, battle: battle)
+    # client = Anthropic::Client.new
+    # content = client.messages(
+    #   parameters: {
+    #     model: "claude-3-haiku-20240307",
+    #     messages: [{ role: "user", content: battle.prompt }],
+    #     max_tokens: 1000
+    #   }
+    # )['content'][0]['text']
+    content = "blablabla"
+    sleep(3)
+    response = battle.responses.find_by(model: "Claude")
+    response.update(content: content)
   end
 end
--- app/jobs/ask_open_ai_job.rb
+++ app/jobs/ask_open_ai_job.rb
@@ -2,13 +2,17 @@ class AskOpenAiJob < ApplicationJob
   queue_as :default
 
   def perform(battle)
-    client = OpenAI::Client.new
-    chatgpt_response = client.chat(
-      parameters: {
-        model: "gpt-4o-mini",
-        messages: [{ role: "user", content: battle.prompt }]
-      }
-    )["choices"][0]["message"]["content"]
-    Response.create(model: "OpenAI", content: chatgpt_response, battle: battle)
+    # client = OpenAI::Client.new
+    # chatgpt_response = client.chat(
+    #   parameters: {
+    #     model: "gpt-4o-mini",
+    #     messages: [{ role: "user", content: battle.prompt }]
+    #   }
+    # )["choices"][0]["message"]["content"]
+    # Response.create(model: "OpenAI", content: chatgpt_response, battle: battle)
+    content = "blablabla"
+    sleep(3)
+    response = battle.responses.find_by(model: "OpenAI")
+    response.update(content: content)
   end
 end
