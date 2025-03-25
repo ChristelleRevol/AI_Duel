0b9ac531a9ab2ae9f11675729f508e6f3154c3d6
Christelle Revol <christellerevol.works@gmail.com>
2025-03-25 09:52:01 +0100

    commit avant pull

--- app/jobs/ask_claude_job.rb
+++ app/jobs/ask_claude_job.rb
@@ -2,24 +2,23 @@ class AskClaudeJob < ApplicationJob
   queue_as :default
 
   def perform
-    raise ClaudeAPI::Error, "Simulated Claude API Error"
-    # response = battle.responses.find_or_create_by(model: "Claude")
-    # begin
-    #   if response.content.nil?
-    #     content = api_response(battle)['content'][0]['text']
-    #     token = api_response(battle)['usage']['input_tokens'] + api_response(battle)['usage']['output_tokens']
-    #     response.update(content: content)
-    #     response.update(token: token)
-    #   end
-    # rescue Anthropic::Error => e
-    #   Rails.logger.error "Anthropic Error: #{e.message}"
-    #   response.update(content: "API Error")
-    # rescue StandardError => e
-    #   Rails.logger.error "General Error : #{e.message}"
-    #   response.update(content: "an unknown error has occurred")
-    # ensure
-    #   broadcast(response, battle)
-    # end
+    response = battle.responses.find_or_create_by(model: "Claude")
+    begin
+      if response.content.nil?
+        content = api_response(battle)['content'][0]['text']
+        token = api_response(battle)['usage']['input_tokens'] + api_response(battle)['usage']['output_tokens']
+        response.update(content: content)
+        response.update(token: token)
+      end
+    rescue Anthropic::Error => e
+      Rails.logger.error "Anthropic Error: #{e.message}"
+      response.update(content: "API Error")
+    rescue StandardError => e
+      Rails.logger.error "General Error : #{e.message}"
+      response.update(content: "an unknown error has occurred")
+    ensure
+      broadcast(response, battle)
+    end
   end
 
   private
@@ -45,5 +44,3 @@ class AskClaudeJob < ApplicationJob
     )
   end
 end
-
-p perform
