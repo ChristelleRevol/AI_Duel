35288ea9726c7af8bde512955abcf832255e29ad
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-03-27 10:35:29 +0100

    added verification + overtime to battle if no vote

--- app/jobs/update_battles_job.rb
+++ app/jobs/update_battles_job.rb
@@ -2,9 +2,16 @@ class UpdateBattlesJob < ApplicationJob
   queue_as :default
 
   def perform
+    return unless Battle.where(winner: nil).where('end_date < ?', DateTime.now).any?
+
     Battle.where(winner: nil).where('end_date < ?', DateTime.now).each do |battle|
-      best_response_id = battle.votes.group(:response_id).count.max_by { |_k, v| v }.first
-      battle.update(winner: Response.find(best_response_id).model)
+      response_votes = battle.votes.group(:response_id).count
+      if response_votes.empty?
+        battle.update(end_date: battle.end_date + 1.hour)
+      else
+        best_response_id = response_votes.max_by { |_k, v| v }.first
+        battle.update(winner: Response.find(best_response_id).model)
+      end
     end
   end
 end
